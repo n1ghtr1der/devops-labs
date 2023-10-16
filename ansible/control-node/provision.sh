831508a01faa5edfed163f7916ed178ac13ae837
Emirton Sousa <emirtonf@gmail.com>
2023-10-16 15:26:35 -0300

    control node removed since control node is the host computer

--- ansible/control-node/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo yum install epel-release -y
-echo "ansible install"
-sudo yum install ansible -y
-cat <<EOT >> /etc/hosts
-192.168.56.4 control-node
-192.168.56.3 db01
-192.168.56.2 app01
-EOT
