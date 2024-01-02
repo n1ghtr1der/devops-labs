3ea93d6d995ec998098c939804eaa27b50ede07c
Emirton Sousa <emirtonf@gmail.com>
2024-01-02 12:19:38 -0300

    feat: sonarqube scripts and setup files

---
+++ sonarqube/infra/sonar-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+useradd sonar
+sudo apt-get install openjdk-11-jdk unzip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+unzip sonarqube-9.1.0.47736.zip -d /opt/
+mv /opt/sonarqube-9.1.0.47736 /opt/sonarqube
+chown -R sonar:sonar /opt/sonarqube
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+sudo unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+sudo mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+chown -R sonar:sonar /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+
+echo '==================== Node Install ===================='
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
