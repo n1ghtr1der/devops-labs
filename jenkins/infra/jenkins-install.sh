0878726585025994c6d488c07622229fcd4bba01
Emirton Sousa <emirtonf@gmail.com>
2024-01-03 13:26:14 -0300

    feat: jenkins lab infrastructure created.

---
+++ jenkins/infra/jenkins-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#this script installs java 11 and jenkins in ubuntu linux instance
+sudo apt update
+
+sudo apt install openjdk-11-jdk -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable --now jenkins
